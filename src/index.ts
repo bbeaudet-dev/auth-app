64d57d83c259974b3bf88b53e0e1fc5460b8b070
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-11 19:37:39 -0400

    Step 1 done

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia"
+import { swagger } from '@elysiajs/swagger'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+export const app = new Elysia()
+  .use(swagger())
+  .get('/api/public/', "This is public information")
+  .get('/api/protected/', "Only admin should be able to see this")
+  .listen(3000)
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
