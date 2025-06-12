9e1ed5bf233f8ef0c3c0270d83917bb2aa240049
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-11 23:06:10 -0400

    Step 2 API documentation complete

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,23 @@ import { Elysia, t } from "elysia"
 import { swagger } from '@elysiajs/swagger'
 
 export const app = new Elysia()
-  .use(swagger())
-  .get('/api/public/', "This is public information")
-  .get('/api/protected/', "Only admin should be able to see this")
+  .use(swagger({path: '/api-docs'}))
+  
+  .get('/api/public/', () => { 
+    return { 
+      message: 'This is public information',
+      access: 'public'
+    }
+  })
+  
+  .get('/api/protected/', () => { 
+    return {
+      message: "Only admin should be able to see this",
+      access: 'protected'
+    }
+  })
+  
+  
   .listen(3000)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
