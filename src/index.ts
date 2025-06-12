bb2287b752ee0728b8f16524176a1e30f9b595ae
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-12 10:33:03 -0400

    introduced secrets and protected endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,49 @@ import { Elysia, status, t } from "elysia"
 import { swagger } from '@elysiajs/swagger'
 
 const users = [ // TODO use database instead of hardcoded array
-  { id: 1, username: "ben", role: "peasant", password: "my-password" },
-  { id: 2, username: "parth", role: "peasant", password: "not-my-password" },
-  { id: 3, username: "andrew", role: "admin", password: "not-his-password" },
-  { id: 4, username: "eugene", role: "basic", password: "not-this-password" },
-  { id: 5, username: "paris", role: "basic", password: "not-that-password" }
-]
+	{ id: 1, username: "ben", role: "peasant", password: "my-password" , secret: "ben-secret" },
+	{ id: 2, username: "parth", role: "peasant", password: "not-my-password", secret: "parth-secret" },
+	{ id: 3, username: "andrew", role: "admin", password: "not-his-password", secret: "andrew-secret" },
+	{ id: 4, username: "eugene", role: "basic", password: "not-this-password", secret: "eugene-secret" },
+	{ id: 5, username: "paris", role: "basic", password: "not-that-password", secret: "paris-secret" }
+] // STEP 6: ^These secrets are not safe! 
 
 export const app = new Elysia()
-  .use(swagger({path: '/api-docs'}))
-  
-  // TODO use derive to pull user info from request rather than from users[]
+	.use(swagger({path: '/api-docs'}))
 
-  // Route for public info
-  .get('/api/public/', () => { 
-    console.log('Public route')
-    return { message: 'This is public information' }
-  })
+	// User Authentication -- "who is this request from?"
+	.derive( (request) => {
+		const headers = request.headers
+		const secret = headers['x-bens-server-secret']
+		if (!secret) return {} // if no secret, user is undefined
+		const user = users.find(user => user.secret === secret)
+		if(!user) return {} // if no matching user, user is undefined
+		console.log("Server: User found")
+		return {user: user} // if matching user, add user context to request
+	})
 
-  // Check for admin access
-  .onBeforeHandle( () => {
-    if (users[0].role !== "admin") { // FIXME Hardcoded user index
-      return status(401)
-    } 
-  })
+	// Authorization -- "is this request allowed?"
+	.onBeforeHandle(({user}) => {
+		if(!user) return status(401)
+	})
 
-  // Route for protected info
-  .get('/api/protected/', () => { 
-    console.log('Protected route')
-    return { message: "Only admin should be able to see this" }
-  })
+	// Route for public info
+	.get('/api/public/', () => { 
+		console.log('Server: This is public information')
+		return { message: 'Client: This is public information' }
+	})
 
-  .listen(3000)
+	// Route for protected info
+	.get(
+		'/api/protected/',
+		({user}) => { 
+			if (user.role !== "admin") return status(401)
+			console.log("Server: Only admin should be able to see this")
+			return { message: "Client: Only admin should be able to see this", user: user }
+		}
+	)
+
+	.listen(3000)
+
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
